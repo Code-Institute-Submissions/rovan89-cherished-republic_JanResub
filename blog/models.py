5360e3d71945b6236e0f9c0ec0ed59778090fa6d
rovan89 <devireland89@gmail.com>
2022-04-17 18:42:19 +0000

    Add Post class

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"),(1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    auther = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
