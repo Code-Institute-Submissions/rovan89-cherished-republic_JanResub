ea6cd5c669fb6d9bae8c13ae61219a414430a9b4
rovan89 <devireland89@gmail.com>
2022-04-22 19:02:53 +0000

    Add content and status fields to todolist model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ToDoList
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(ToDoList)
+class ToDoListAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'status', 'created_on')
+    summernote_fields = ('content')
+
