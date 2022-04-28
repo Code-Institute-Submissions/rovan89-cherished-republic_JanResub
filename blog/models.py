ce27ac011bc00f394ddbcc57b5124de24c842744
rovan89 <devireland89@gmail.com>
2022-04-28 20:46:30 +0000

    Clean up code

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ STATUS = ((0, "Draft"),(1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    auther = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
