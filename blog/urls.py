d820edc7f498d76f2cc6744a3157bd0f078cf26e
rovan89 <devireland89@gmail.com>
2022-04-28 20:44:29 +0000

    Add path for PostDetail

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="mainTask")
+    path('', views.PostList.as_view(), name="mainTask"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
