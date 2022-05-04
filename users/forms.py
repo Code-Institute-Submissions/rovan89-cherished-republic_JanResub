069c623ea84a91f5cd24ac6555bcbd02f3589c6d
rovan89 <devireland89@gmail.com>
2022-05-04 21:26:22 +0000

    Clean up code

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class NewUserForm(UserCreationForm):
+    """
+    This class has customised the UserCreationForm.
+    The four user inputs are 'username', 'email', 'password1', 'password2'
+    Password1 is validated by retyping it into password2).
+    """
     email = forms.EmailField()
 
     class Meta:
-        model = User()
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', blog_views.PostList.as_view(), name="home"),
+    path('home/', blog_views.PostList.as_view(), name="home"),
 ]
