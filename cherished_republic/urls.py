711bdff72ca07d068091322af3101acec39ae0af
rovan89 <devireland89@gmail.com>
2022-05-04 20:26:17 +0000

    Add path to users.urls

--- cherished_republic/urls.py
+++ cherished_republic/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import todo.urls
 import blog.urls
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
-    path('#', include('todo.urls'), name='todo_urls'),
+    path('users/', include('users.urls'), name='users_urls'),
+
 ]
