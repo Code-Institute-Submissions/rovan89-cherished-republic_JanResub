fe121725df1d1abd2c3adf38aa6f902f4dabc67b
rovan89 <devireland89@gmail.com>
2022-05-05 14:12:29 +0000

    Add desciption comment to CommentForm class

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,10 @@ from .models import Comment
 from django import forms
 
 class CommentForm(forms.ModelForm):
-
+    """
+    This class is the model for users comments.
+    It inherits from the Comment class in models.py
+    """
     class Meta:
         model = Comment
         fields = ('body',)
