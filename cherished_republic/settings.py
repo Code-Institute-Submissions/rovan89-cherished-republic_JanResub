5a9be8430a5cddc58148fe347b85e81f5b5bb305
rovan89 <devireland89@gmail.com>
2022-05-05 12:29:54 +0000

    Add django_form to installed apps and CRIPSY_TEMPLATE_PACK for styling

--- cherished_republic/settings.py
+++ cherished_republic/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'django_forms',
     'blog',
     'todo',
     'users',
@@ -62,6 +63,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
