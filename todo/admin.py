4463d43be36f84e3abc19f569703dc53c2f2dd93
rovan89 <devireland89@gmail.com>
2022-05-08 17:46:24 +0000

    Add prepopulated_fields to ToDoListAdmin

--- todo/admin.py
+++ todo/admin.py
@@ -5,6 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(ToDoList)
 class ToDoListAdmin(admin.ModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'created_on')
     summernote_fields = ('content')
 
