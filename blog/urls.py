91a2f56c357078c43dc3e8fb216f695e59a74d9f
rovan89 <devireland89@gmail.com>
2022-05-11 20:15:15 +0000

    Add urls for create_post, update_post and delete_post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
+    path('create_post/', views.create_user_post, name='create_post'),
+    path('update_post/<post_id>', views.update_user_post, name='update_post'),
+    path('delete_post/<post_id>', views.delete_post, name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
