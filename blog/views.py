7309ebc3112b0609ac1bd34a1d4e4708f2191fcd
rovan89 <devireland89@gmail.com>
2022-05-11 20:14:11 +0000

    Add functions for users to create, update and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,61 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.utils.text import slugify
 from .models import Post
-from .forms import CommentForm
+from .forms import CommentForm, UserPostForm
+
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html"
     paginate_by = 6
-    
+
+
+def create_user_post(request):
+    """
+    This function allows a user to create a post,
+    and updates the database. 
+    """
+    form = UserPostForm()
+    context = {
+        'form': form
+    }
+
+    if request.method == 'POST':
+        form = UserPostForm(request.POST)
+        if form.is_valid():
+            user_form = form.save(commit=False)
+            user_form.slug = slugify(user_form.title)
+            user_form.save()
+            return redirect('home')
+    return render(request, "create_post.html", context)
+
+
+def update_user_post(request, post_id):
+    """
+    This function allows the user to update a post.
+    """
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = UserPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = UserPostForm(instance=post)
+    context = {
+        'form': form
+    }
+    return render(request, 'update_post.html', context)
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('home')
+
 
 class PostDetail(View):
     """
