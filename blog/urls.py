bdb89c2c3391c59d829f24ebe88713a43e0c5493
rovan89 <devireland89@gmail.com>
2022-05-12 11:24:35 +0000

    Add url for search path

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create_post/', views.create_user_post, name='create_post'),
     path('update_post/<post_id>', views.update_user_post, name='update_post'),
     path('delete_post/<post_id>', views.delete_post, name='delete_post'),
+    path('search_posts/', views.post_search, name='search_posts'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
