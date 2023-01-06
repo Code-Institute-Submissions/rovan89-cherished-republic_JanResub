d83d25b2c37cf33e18a58d6616353b62c33602db
rovan89 <devireland89@gmail.com>
2023-01-06 12:16:54 +0000

    Add static file root to urlpatterns

--- cherished_republic/urls.py
+++ cherished_republic/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,4 @@ urlpatterns = [
     path('todo/', include('todo.urls'), name='todo_urls'),
     path('accounts/', include('allauth.urls')),
     path('', include('blog.urls'), name='blog_urls'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
