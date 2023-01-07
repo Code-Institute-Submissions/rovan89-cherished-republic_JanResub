32a802080f9110375044df661198f3eef63f8ab7
rovan89 <devireland89@gmail.com>
2023-01-07 19:30:46 +0000

    Add profile urls

--- cherished_republic/urls.py
+++ cherished_republic/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('todo/', include('todo.urls'), name='todo_urls'),
+    path('profiles/', include('profiles.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('blog.urls'), name='blog_urls'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
