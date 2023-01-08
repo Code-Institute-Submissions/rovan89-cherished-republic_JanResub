570ff9946f12543b41e9303fc567bd9bfd24b776
rovan89 <devireland89@gmail.com>
2023-01-08 14:05:47 +0000

    Add DeleteProfileView

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
 
 from .models import Profile
 
@@ -10,7 +12,14 @@ class UpdateUserProfileView(UpdateView):
     """ View to update user profiles """
     model = Profile
     template_name = 'update_profile.html'
-    fields = '__all__'
+    fields = ('first_name', 'last_name', 'bio', 'email', 'phone_number')
     success_url = reverse_lazy('home')
 
-
+class DeleteProfileView(SuccessMessageMixin, DeleteView):
+    """ View to delete profile """
+    model = User
+    template_name = "delete_profile.html"
+    success_message = "User profile has been deleted"
+    success_url = reverse_lazy('home')
+     
+    
