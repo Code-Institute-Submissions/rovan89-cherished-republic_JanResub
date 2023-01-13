97fea70c1e75e10a8337039f66fa4bc0aad34120
rovan89 <devireland89@gmail.com>
2023-01-13 12:35:05 +0000

    Change user and phone number fields

--- profiles/models.py
+++ profiles/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.urls import reverse
+
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+
 class Profile(models.Model):
     """
     This a cumstom user profile class.
     """
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, default='Enter your bio here')
     first_name = models.CharField(max_length=50, null=True, blank=True)
     last_name = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
