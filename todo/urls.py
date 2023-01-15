c220eb9978b22cb5bb0a0ac7749f972cb0214883
rovan89 <devireland89@gmail.com>
2023-01-15 10:59:44 +0000

    Remove todo app

--- todo/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('todo_list/', views.TodoItems.as_view(), name='todo_list'),
-    #path('todo/', views.ToDoList.as_view, name="todo_list"),
-]
